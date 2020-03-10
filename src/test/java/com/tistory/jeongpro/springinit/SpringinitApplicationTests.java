867c6cb211452b02a61696eea2d5c527506c5a1c
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-10 20:06:42 +0900

    create springboot project (dependency : web, devtool)

---
+++ src/test/java/com/tistory/jeongpro/springinit/SpringinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jeongpro.springinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
