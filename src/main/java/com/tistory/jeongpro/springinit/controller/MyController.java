044ab70656cb25e9aaf2f1369a5ee63905bace7e
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-10 22:43:26 +0900

    properties에 color 적용 및 빈 주입 테스트를 위한 클래스 생성

--- src/main/java/com/tistory/jeongpro/springinit/controller/MyController.java
+++ src/main/java/com/tistory/jeongpro/springinit/controller/MyController.java
@@ -1,4 +1,10 @@
 package com.tistory.jeongpro.springinit.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+    public void hello(){
+        System.out.println("Hello MyController");
+    }
 }
