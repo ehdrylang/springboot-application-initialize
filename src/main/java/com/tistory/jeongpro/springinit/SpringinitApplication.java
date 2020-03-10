82ff0cd748437615dbf482120e6e1f4734937b04
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-10 20:37:52 +0900

    가장 간단한 방법 main 메서드에 직접 코드 사용

--- src/main/java/com/tistory/jeongpro/springinit/SpringinitApplication.java
+++ src/main/java/com/tistory/jeongpro/springinit/SpringinitApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringinitApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringinitApplication.class, args);
+		System.out.println("Hello Spring Boot Application");
 	}
-
 }
