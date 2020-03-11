47bb52a1124464c299b97e2368ddedd495276af6
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-11 22:33:55 +0900

    다시 @EventListener 로 변경하기

--- src/main/java/com/tistory/jeongpro/springinit/SpringinitApplication.java
+++ src/main/java/com/tistory/jeongpro/springinit/SpringinitApplication.java
@@ -5,14 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SpringinitApplication {
 	public static void main(String[] args) {
 		//ConfigurableApplicationContext : ApplicationContext를 코드에 의해서 재구성할 수 있도록 기능을 집어넣은 ApplicationContext.
 		ConfigurableApplicationContext ac = SpringApplication.run(SpringinitApplication.class, args);
-		ac.addApplicationListener((ApplicationListener<MyEvent>)event-> System.out.println("Hello ApplicationListener : " + event.getMessage()));
 		ac.publishEvent(new MyEvent(ac,"My Spring Event"));
+	}
+	@EventListener(MyEvent.class)
+	public void onMyEvent(MyEvent myEvent){
+		System.out.println("Hello EventListener : " + myEvent.getMessage());
 	}
 	static class MyEvent extends ApplicationEvent {
 		private final String message;
