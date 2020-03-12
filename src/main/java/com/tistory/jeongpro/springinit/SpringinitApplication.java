4f3947794e773ed4ed8316d261d8ec42b076df5a
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-12 20:50:42 +0900

    학습 끝

--- src/main/java/com/tistory/jeongpro/springinit/SpringinitApplication.java
+++ src/main/java/com/tistory/jeongpro/springinit/SpringinitApplication.java
@@ -2,33 +2,24 @@ package com.tistory.jeongpro.springinit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @SpringBootApplication
 public class SpringinitApplication {
 	public static void main(String[] args) {
-		//ConfigurableApplicationContext : ApplicationContext를 코드에 의해서 재구성할 수 있도록 기능을 집어넣은 ApplicationContext.
 		ConfigurableApplicationContext ac = SpringApplication.run(SpringinitApplication.class, args);
-		ac.publishEvent(new MyEvent(ac,"My Spring Event"));
 	}
-	@EventListener(MyEvent.class)
-	public void onMyEvent(MyEvent myEvent){
-		System.out.println("Hello EventListener : " + myEvent.getMessage());
-	}
-	static class MyEvent extends ApplicationEvent {
-		private final String message;
-
-		//원래 String message는 없는 건데 추가한 것임.
-		public MyEvent(Object source, String message) {
-			super(source);
-			this.message = message;
-		}
-		public String getMessage(){
-			return message;
-		}
+	@EventListener(ApplicationReadyEvent.class)
+	public void onApplicationReadyEvent(){
+		System.out.println("Application Ready");
 	}
 }
-
